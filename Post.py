88e87af8dcb2e27be0d48a51effe51a712edb23e
krkannan94 <krkannan94@gmail.com>
2020-08-03 22:47:49 +0800

    Update Post.py

--- Post.py
+++ Post.py
@@ -2,10 +2,10 @@ import tweepy
 from time import sleep
  
 # personal information 
-api_key = "Znxbo7JW3ts58vFitj6na8HV"
-api_secret_key = "mD8mQxlhvrcsSo5Ql0wNPnFz74y47cYhHc4xJlY1Z8yUFi4is"
-access_token = "128303907221933315-UEjF4tZWQSYgFdAeGeNEfihPSissQT"
-access_token_secret = "gfMFlN5dXSUD40C3ZFmdhoUHtB6qkKz69VNRO2VPvHLJ"
+api_key = "Znxbo7JW3tts58vFitj6na8HV"
+api_secret_key = "mD8mQxlhyvrcsSo5Ql0wNPnFz74y47cYhHc4xJlY1Z8yUFi4is"
+access_token = "1283039072219533315-UEjF4tZWQSYgFdAeGeNEfihPSissQT"
+access_token_secret = "gfMFlN5dlXSUD40C3ZFmdhoUHtB6qkKz69VNRO2VPvHLJ"
 
 # authentication 
 auth = tweepy.OAuthHandler(api_key, api_secret_key) 
@@ -15,10 +15,10 @@ auth.set_access_token(access_token, access_token_secret)
 # authentication of access token and secret 
 api = tweepy.API(auth) 
 
-for i in range(10):
+for i in iter(int, 1):
 
-	for tweet in tweepy.Cursor(api.search, q='its  -filter:retweets -filter:replies', result_type='recent', lang='en').items(1): 
-		try: 
+	for tweet in tweepy.Cursor(api.user_timeline, screen_name='guroomoorthy', include_rts=False, include_replies=False).items(1): 
+		try:
 			print('\nTweet by: @' + tweet.user.screen_name) 
 
 			if not tweet.retweeted:
@@ -31,4 +31,4 @@ for i in range(10):
 
 		except:
 			print('Already retweeted and favorited please be patient till next tweet')
-			sleep(60)
+			sleep(300)
